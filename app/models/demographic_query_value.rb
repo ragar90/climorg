bc9f69ab73c04f012c59a4deff5307012bf9f7ba
jhlive <jhlive300590@gmail.com>
2013-09-06 00:34:10 -0600

    Adding last methods for query results

--- app/models/demographic_query_value.rb
+++ app/models/demographic_query_value.rb
@@ -1,14 +1,23 @@
 class DemographicQueryValue
-  attr_accessor :condition_value
-  attr_accessor :variable_type
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+  
+  attr_accessor :condition_value, :variable_type
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
   
   def to_qprm
     unless condition_value
       ""
     else
       conditions =  self.query_values
-      if conditions.length > 1
-          ["demographic_values.value = ?", conditions]
+      if conditions.length == 1
+          ["demographic_values.value = ?", conditions.first]
       else
         case variable_type
         when "hash"
@@ -24,13 +33,13 @@ class DemographicQueryValue
   def query_values
     case condition_value
     when /\d+-\d+/ #Number
-      @query_values || = condition_value.split("-").map{|d| d.to_i }.sort
+      @query_values ||= condition_value.split("-").map{|d| d.to_i }.sort
     when /^(\d+\.\d+)+\-(\d+\.\d+)+$/
-      @query_values || = condition_value.split("-").map{|d| d.to_f }.sort
+      @query_values ||= condition_value.split("-").map{|d| d.to_f }.sort
     when /(\w+,)|\w+/#Hash
-      @query_values || = condition_value.split(",")
+      @query_values ||= condition_value.split(",")
     else #Exact Value
-      @query_values || = condition_value
+      @query_values ||= [condition_value]
     end
   end
 end
