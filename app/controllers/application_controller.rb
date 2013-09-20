b540837c9fc0cb1dbad6c11ab92cb18caca5f802
jhlive <jhlive300590@gmail.com>
2013-09-20 17:48:47 -0600

    Adding new layout design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :load_layout
+  before_action :selected_option
 
   def load_layout
   	@layout = (params[:layout].nil? or params[:layout]==true) ? true : false
@@ -10,5 +11,20 @@ class ApplicationController < ActionController::Base
     klass = resource.to_s.classify.constantize
     params.require(resource.to_sym).permit!
     #params.require(resource.to_sym).permit(klass.whitelist_attributes)
+  end
+
+  def selected_option
+     @selected_option = case params[:controller]
+                          when "demographic_variables"
+                            "demo-vars-option"
+                          when "dimensions"
+                            "dimension-option"
+                          when "questions"
+                            "questions-option"
+                          when "settings"
+                            "settings-option"
+                          else
+                            "home-option"
+                        end
   end
 end
